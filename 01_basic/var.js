4e0d971957dc9de83aef3233101c8e682e718c15
Najmun-git <81632431+Najmun-git@users.noreply.github.com>
2024-08-18 13:38:50 +0000

    added basic folder

---
+++ 01_basic/var.js
@@ -0,0 +1,24 @@
+What is Good?
+let and const have block scope.
+
+let and const can not be redeclared.
+
+let and const must be declared before use.
+
+let and const does not bind to this.
+
+let and const are not hoisted.
+
+
+var x = 2;   // Allowed
+let x = 3;   // Not allowed
+
+{
+let x = 2;   // Allowed
+let x = 3;   // Not allowed
+}
+
+{
+let x = 2;   // Allowed
+var x = 3;   // Not allowed
+}
