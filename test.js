2ba4cb87563c89767a6c97d5a89b1d3656b7fb7d
Najmun-git <81632431+Najmun-git@users.noreply.github.com>
2024-08-18 13:35:19 +0000

    variable added

---
+++ test.js
@@ -0,0 +1,18 @@
+const accountId= 12345;
+var accountName = "BOB";
+let userMail ="najmun@google.com";
+
+accountCity="noida";
+accountName ="SBI";
+userMail ="najmun@yahoo.com"
+console.table([accountId, accountName, accountCity, userMail]);
+
+for(let i=0; i<=5; i++){
+    setTimeout(()=>{console.log(i)},0)
+}
+/* new line */
+    for(var i=0; i<5; i++){
+        setTimeout(()=>{console.log(i)},0)
+    }    
+
+    
